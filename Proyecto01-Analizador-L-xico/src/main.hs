c7c3626d8e7110e52ec202030abc7139a694a1a5
DanielaSanluis <danielasanluic@ciencias.unam.mx>
2025-10-09 02:54:23 -0600

    Se modifico el main para probar AFN entonces funciona AFN -> AFD

--- Proyecto01-Analizador-L-xico/src/main.hs
+++ Proyecto01-Analizador-L-xico/src/main.hs
@@ -3,6 +3,7 @@ module Main where
 import ER
 import AFNEp
 import AFN
+import AFD
 
 -- Ejemplo de expresiones regulares:
 -- 1. (a+b)*c  → cualquier secuencia de a's y b's seguida de una c
@@ -17,34 +18,43 @@ main = do
     let expr1 = And (Kleene (Or (Term 'a') (Term 'b'))) (Term 'c')
     let (afnEps1, _) = expr_to_AFNEp expr1 0
     let afn1 = afnEp_to_AFN afnEps1
+    let afd1 = afn_to_afd afn1
     putStrLn "\nExpresion regular: (a+b)*c"
     print expr1
     putStrLn "\nAFNε resultante:"
     printAFNEp afnEps1
     putStrLn "\n AFN (sin ε):"
     printAFN afn1
+    putStrLn "\n AFD (determinista):"
+    printAFD afd1
 
     -- Ejemplo 2: [a-z]
     let expr2 = Range 'a' 'z'
     let (afnEps2, _) = expr_to_AFNEp expr2 0
     let afn2 = afnEp_to_AFN afnEps2
+    let afd2 = afn_to_afd afn2
     putStrLn "\nExpresion regular: [a-z]"
     print expr2
     putStrLn "\nAFNε resultante:"
     printAFNEp afnEps2
     putStrLn "\n AFN (sin ε):"
     printAFN afn2
+    putStrLn "\n AFD (determinista):"
+    printAFD afd2
 
     -- Ejemplo 3: a(b|c)
     let expr3 = And (Term 'a') (Or (Term 'b') (Term 'c'))
     let (afnEps3, _) = expr_to_AFNEp expr3 0
     let afn3 = afnEp_to_AFN afnEps3
+    let afd3 = afn_to_afd afn3
     putStrLn "\nExpresion regular: a(b|c)"
     print expr3
     putStrLn "\nAFNε resultante:"
     printAFNEp afnEps3
     putStrLn "\n AFN (sin ε):"
     printAFN afn3
+    putStrLn "\n AFD (determinista):"
+    printAFD afd3
 
 -- Función auxiliar para imprimir bonito el AFNε
 printAFNEp :: AFNEp -> IO ()
@@ -69,4 +79,16 @@ printAFN m = do
     putStrLn $ "Estados finales: " ++ show (finalesN m)
   where
     printTrans (q, c, qs) = putStrLn $ "  " ++ q ++ " --" ++ [c] ++ "--> " ++ show qs
+
+printAFD :: AFD -> IO ()
+printAFD m = do
+    putStrLn $ "Estados: " ++ show (estadosD m)
+    putStrLn $ "Alfabeto: " ++ show (alfabetoD m)
+    putStrLn "Transiciones:"
+    mapM_ printTrans (transicionesD m)
+    putStrLn $ "Estado inicial: " ++ inicialD m
+    putStrLn $ "Estados finales: " ++ show (finalesD m)
+  where
+    printTrans (q, c, q2) = putStrLn $ "  " ++ q ++ " --" ++ [c] ++ "--> " ++ q2
+
 
